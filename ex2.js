5a01d4b557758a604647e8b533e2f2451edcad58
DERIVALDO12 <rhuan.toigo@escola.pr.gov.br>
2026-05-10 15:37:19 -0300

    terminei a 4

---
+++ ex2.js
@@ -0,0 +1,6 @@
+const input = require('readline-sync')
+let num1 = input.question()
+let num2 = input.question()
+let soma = num1 + num2
+soma = parseInt(num1) + parseInt(num2)
+console.log(soma)
---
+++ ex4.js
@@ -0,0 +1,9 @@
+const input = require('readline-sync')
+let num1 = input.question()
+for( let i = 1; i <= 10; i++) {
+console.log(`${num1} x ${i} = ${num1 * i}`);
+}
+
+
+
+
